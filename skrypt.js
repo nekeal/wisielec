bc440657f7ddbe0bef3432df19c1906092a943b0
Szymon Cader <szymon.sc.cader@gmail.com>
2017-05-28 22:02:40 +0200

    added responsive and fast image load

--- skrypt.js
+++ skrypt.js
@@ -16,27 +16,47 @@ $(function () {
     }
 
     function wypiszLitery() {
-        var lit="";
+        var lit = document.createDocumentFragment();
+        var div = '<div class="row"></div>';
         var pojemnik = $('#litery');
         for (i = 0; i < 35; i++) {
-            lit.append('<div class="litera">' + litery[i] + "</div>")
-            if (i > 0 && (i + 1) % 7 == 0)
-                pojemnik.append('<div class="row"</div>');
+            // $(lit).append('<div class="col-xs-1 litera">' + litery[i] + "</div>");
+            $(lit).append('<div class="cl-1"><div class="litera">' + litery[i] + "</div></div>");
+            // pojemnik.append('<div class="litera">' + litery[i] + "</div>");
+            if (i > 0 && (i + 1) % 7 == 0) {
+                //$(lit).append('<div class="col-xs-5 zero" </div>');              
+                pojemnik.append(div);
+                $(".row").last().append(lit);
+                // lit.appendTo($(".row").last());
+                lit = document.createDocumentFragment();
+                // pojemnik.append('<div style="clear:both;"</div>')
+            }
+        }
+    }
+
+    function loadImage(ile) {
+        for (i = 0; i < ile; i++) {
+            images[i] = new Image();
+            images[i].src='img/s'+i+".jpg";
+            images[i].alt="Something went wrong"
         }
     }
 
     function image(id) {
-        $("#szubienica").html("<img class='img img-responsive' src=img/s" + id + ".jpg alt=''>");
+        // $("#szubienica").html("<img class='img img-responsive' src=img/s" + id + ".jpg alt=''>");
+        $("#szubienica").html(images[id]);
     }
     String.prototype.replaceAt = function (a, b) {
         return this.substr(0, a) + b + this.substr(a + 1);
     };
+    var images = [];
     var litery = ["A", "Ą", "B", "C", "Ć", "D", "E", "Ę", "F", "G", "H", "I", "J", "K", "L", "Ł", "M", "N", "Ń", "O", "Ó", "P", "Q", "R", "S", "Ś", "T", "U", "V", "W", "X", "Y", "Z", "Ź", "Ż"];
     var haslo = "KOSZ NA ŚMIECI";
     var id = 0;
     var pomoc = naSpacje(haslo);
     wypiszHaslo(pomoc);
     wypiszLitery();
+    loadImage(9);
     image(id);
 
     $(".litera").on("click", function () {
@@ -51,13 +71,12 @@ $(function () {
             }
 
         }
-        if(haslo==pomoc)
-        {
-                $("#litery").html("<button id='retry' class='btn btn-lg btn-info' type='button'>Zagraj Ponownie!</button>");
-                $("#litery").prepend("Gratulacje, wygrałeś</br>");
-                $("#retry").on("click", function(){
-                    location.reload();
-                });
+        if (haslo == pomoc) {
+            $("#litery").html("<button id='retry' class='btn btn-lg btn-info' type='button'>Zagraj Ponownie!</button>");
+            $("#litery").prepend("Gratulacje, wygrałeś</br>");
+            $("#retry").on("click", function () {
+                location.reload();
+            });
         }
         if (!$(this).hasClass("clicked-good")) {
             $(this).removeClass("litera");
@@ -68,7 +87,7 @@ $(function () {
                 $("#litery").html("<button id='retry' class='btn btn-lg btn-info' type='button'>Spróbuj jeszcze raz!</button>");
                 $("#litery").prepend("Niestety nie udało się, zostałeś powieszony</br>");
 
-                $("#retry").on("click", function(){
+                $("#retry").on("click", function () {
                     location.reload();
                 });
             }
